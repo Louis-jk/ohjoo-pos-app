49154f023ea3d2c42c462f901e9438e67325b6ed
Juno Kim <lippoint.surf0622@gmail.com>
2021-08-09 11:37:14 +0900

    주문거부/주문취소 리스트 별도 생성

---
+++ src/assets/datas/orders.ts
@@ -0,0 +1,51 @@
+  // 주문 취소 선택 값
+  export const cancelInitState = [
+    {
+      label: '고객 요청',
+      value: '1'
+    },
+    {
+      label: '업소 사정',
+      value: '2'
+    },
+    {
+      label: '배달 불가',
+      value: '3'
+    },
+    {
+      label: '재료 소진',
+      value: '4'
+    },
+    {
+      label: '기타',
+      value: '5'
+    }
+  ]
+
+  // 주문 거부 선택 값
+  export const rejectInitState = [
+    {
+      label: '배달 불가 지역',
+      value: '1'
+    },
+    {
+      label: '메뉴 및 업소 정보 다름',
+      value: '2'
+    },
+    {
+      label: '재료 소진',
+      value: '3'
+    },
+    {
+      label: '배달 지연',
+      value: '4'
+    },
+    {
+      label: '고객 정보 부정확',
+      value: '5'
+    },
+    {
+      label: '기타',
+      value: '6'
+    },
+  ]
