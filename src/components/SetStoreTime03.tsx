831e5edcf188b9dd5a40b660a93db41adb193739
Juno Kim <lippoint.surf0622@gmail.com>
2021-08-09 10:25:06 +0900

    캘린더 추가

--- src/components/SetStoreTime03.tsx
+++ src/components/SetStoreTime03.tsx
@@ -1,18 +1,76 @@
-import React from 'react';
-import { Box, Typography } from '@material-ui/core';
-import Header from '../components/Header';
-import { theme, MainBox, baseStyles, ModalCancelButton, ModalConfirmButton } from '../styles/base';
+import React, { useState } from 'react';
+import moment from 'moment';
+import { theme, baseStyles } from '../styles/base';
 
-export default function SetStoreTime01() {
+import DayPicker, { LocaleUtils, DateUtils } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+import MomentLocaleUtils from 'react-day-picker/moment';
+import 'moment/locale/ko';
+
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import { Typography, IconButton } from '@material-ui/core';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+
+function formatMonthTitle(d: any, locale: any) {
+  return `${d.getFullYear()}년 ${d.getMonth() + 1}월`
+}
+
+export default function StoreTimeTab03() {
 
   const base = baseStyles();
+  const [selectedDays, setSelectedDays] = useState([]);
+
+
+  const modifiers = {
+    thursdays: { daysOfWeek: [4] },
+    birthday: new Date(2018, 9, 30),
+
+  };
+
+  const modifiersStyles = {
+    birthday: {
+      color: 'white',
+      backgroundColor: theme.palette.primary.main,
+    },
+    // thursdays: {
+    //   color: '#ffc107',
+    //   backgroundColor: '#fffdee',
+    // },
+  }
+
+  const handleDayClick = (day: any) => {
+
+    let formatDay = moment(day).format('YYYY-MM-DD');
+
+    const filtered = selectedDays.find(selectedDay => moment(selectedDay).format('YYYY-MM-DD') === formatDay);
+    console.log("filtered ?", filtered);
+
+    if (filtered) {
+      const removeObj = selectedDays.filter(selectedDay => moment(selectedDay).format('YYYY-MM-DD') !== formatDay);
+      console.log("removeObj", removeObj);
+      setSelectedDays(removeObj);
+    } else {
+      let result = selectedDays.concat(day);
+      setSelectedDays(result);
+    }
+  }
+
+  console.log("selectedDays ???", selectedDays);
 
   return (
-    <Box component="div" className={base.root}>
-      <Header />
-      <MainBox component='main' sx={{ flexGrow: 1, p: 3 }}>
-        <Typography>안녕하세요03</Typography>
-      </MainBox>
-    </Box>
+    // <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" width="100%">
+
+    <DayPicker
+      localeUtils={{ ...MomentLocaleUtils, formatMonthTitle }}
+      locale="ko"
+      selectedDays={selectedDays}
+      onDayClick={handleDayClick}
+      modifiers={modifiers}
+      modifiersStyles={modifiersStyles}
+    />
+
+    // </Box >
   )
 }
