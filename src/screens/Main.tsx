82a9433fb4b18f3035f16104ef424c117a406153
Juno Kim <lippoint.surf0622@gmail.com>
2021-08-12 17:01:53 +0900

    현재 주문건 Api 통신 및 redux 스토어 저장

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -11,6 +11,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 // Local Component
 import Api from '../Api';
 import storeAction from '../redux/actions';
+import orderAction from '../redux/actions';
 import { baseStyles, theme } from '../styles/base';
 
 
@@ -21,23 +22,7 @@ export default function Main(props: any) {
   const history = useHistory();
   const { mt_id, mt_jumju_code } = useSelector((state: any) => state.login);
 
-  const [audio] = React.useState(new Audio('https://dmonster1452.cafe24.com/api/sound.mp3'));
-  const [playing, setPlaying] = React.useState(false);
-  const toggle = () => setPlaying(!playing);
-
-  useEffect(() => {
-    playing ? audio.play() : audio.pause();
-  },
-    [playing]
-  );
-
-  useEffect(() => {
-    audio.addEventListener('ended', () => setPlaying(false));
-    return () => {
-      audio.removeEventListener('ended', () => setPlaying(false));
-    };
-  }, []);
-
+  // 등록된 스토어 가져오기
   const getStoreHandler = () => {
     const param = {
       jumju_id: mt_id,
@@ -52,13 +37,121 @@ export default function Main(props: any) {
       console.log("store list", arrItems);
       if (resultItem.result === 'Y') {
         // let initialSelectStore = arrItems.filter(store => store.mt_id === mt_id);
-
         dispatch(dispatch(storeAction.updateStore(arrItems)));
+        getNewOrderHandler();
         // dispatch(dispatch(storeAction.selectStore(initialSelectStore[0].id, initialSelectStore[0].mt_jumju_id, initialSelectStore[0].mt_jumju_code, initialSelectStore[0].mt_store, initialSelectStore[0].mt_addr)));
+      } else {
+        console.log('arrItems', arrItems);
+        dispatch(dispatch(storeAction.updateStore([])));
+        getNewOrderHandler();
       }
-      history.push('/order_new');
+
     });
   };
+
+  // 현재 신규주문 건수 가져오기
+  const getNewOrderHandler = () => {
+
+    const param = {
+      item_count: 0,
+      limit_count: 10,
+      jumju_id: mt_id,
+      jumju_code: mt_jumju_code,
+      od_process_status: '신규주문'
+    };
+    Api.send('store_order_list', param, (args: any) => {
+      let resultItem = args.resultItem;
+      let arrItems = args.arrItems;
+
+      if (resultItem.result === 'Y') {
+        console.log("신규주문 success?", arrItems);
+        dispatch(dispatch(orderAction.updateNewOrder(JSON.stringify(arrItems))));
+        getCheckOrderHandler();
+      } else {
+        console.log("신규주문 faild?", arrItems);
+        dispatch(dispatch(orderAction.updateNewOrder(null)));
+        getCheckOrderHandler();
+      }
+    });
+  }
+
+  // 현재 접수완료 주문 가져오기
+  const getCheckOrderHandler = () => {
+
+    const param = {
+      item_count: 0,
+      limit_count: 10,
+      jumju_id: mt_id,
+      jumju_code: mt_jumju_code,
+      od_process_status: '접수완료'
+    };
+    Api.send('store_order_list', param, (args: any) => {
+      let resultItem = args.resultItem;
+      let arrItems = args.arrItems;
+
+      if (resultItem.result === 'Y') {
+        console.log("접수완료 success?", arrItems);
+        dispatch(dispatch(orderAction.updateCheckOrder(JSON.stringify(arrItems))));
+        getDeliveryOrderHandler();
+      } else {
+        console.log("접수완료 faild?", arrItems);
+        dispatch(dispatch(orderAction.updateCheckOrder(null)));
+        getDeliveryOrderHandler();
+      }
+    });
+  }
+
+  // 현재 접수완료 주문 가져오기
+  const getDeliveryOrderHandler = () => {
+
+    const param = {
+      item_count: 0,
+      limit_count: 10,
+      jumju_id: mt_id,
+      jumju_code: mt_jumju_code,
+      od_process_status: '배달중'
+    };
+    Api.send('store_order_list', param, (args: any) => {
+      let resultItem = args.resultItem;
+      let arrItems = args.arrItems;
+
+      if (resultItem.result === 'Y') {
+        console.log("배달중 success?", arrItems);
+        dispatch(dispatch(orderAction.updateDeliveryOrder(JSON.stringify(arrItems))));
+        getDoneOrderHandler();
+      } else {
+        console.log("배달중 faild?", arrItems);
+        dispatch(dispatch(orderAction.updateDeliveryOrder(null)));
+        getDoneOrderHandler();
+      }
+    });
+  }
+
+  // 현재 접수완료 주문 가져오기
+  const getDoneOrderHandler = () => {
+
+    const param = {
+      item_count: 0,
+      limit_count: 10,
+      jumju_id: mt_id,
+      jumju_code: mt_jumju_code,
+      od_process_status: '배달완료'
+    };
+    Api.send('store_order_list', param, (args: any) => {
+      let resultItem = args.resultItem;
+      let arrItems = args.arrItems;
+
+      if (resultItem.result === 'Y') {
+        console.log("배달완료 success?", arrItems);
+        dispatch(dispatch(orderAction.updateDoneOrder(JSON.stringify(arrItems))));
+        history.push('/order_new');
+      } else {
+        console.log("배달완료 faild?", arrItems);
+        dispatch(dispatch(orderAction.updateDoneOrder(null)));
+        history.push('/order_new');
+      }
+    });
+  }
 
   useEffect(() => {
     getStoreHandler();
