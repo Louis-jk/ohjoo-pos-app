b72bde1ff8b455589430ab565c5acb5a4a8c2d73
Juno Kim <lippoint.surf0622@gmail.com>
2021-08-19 14:29:52 +0900

    리스트 유무 스타일 추가

--- src/screens/MenuList.tsx
+++ src/screens/MenuList.tsx
@@ -109,54 +109,58 @@ export default function MenuList(props: any) {
         :
         <MainBox component='main' sx={{ flexGrow: 1, p: 3 }}>
           <Box mt={3} />
-          <Grid container spacing={3} style={{ minHeight: 550 }}>
-            {lists && lists.length > 0 && lists.map((list, index) => (
-              <Grid key={list.it_id} item xs={12} md={6} alignContent='baseline'>
-                <Link to={`/menu_edit/${list.it_id}`}>
-                  <Paper className={menu.menuListPaper}>
-                    <Box className={menu.menuListWrap}>
-                      {list.it_img1 &&
-                        <Box>
-                          <img src={list.it_img1} className={menu.menuListImg} alt={list.it_name} />
+          {lists && lists.length > 0 &&
+            <Grid container spacing={3} style={{ minHeight: 550 }}>
+              {lists.map((list, index) => (
+                <Grid key={list.it_id} item xs={12} md={6} alignContent='baseline'>
+                  <Link to={`/menu_edit/${list.it_id}`}>
+                    <Paper className={menu.menuListPaper}>
+                      <Box className={menu.menuListWrap}>
+                        {list.it_img1 &&
+                          <Box>
+                            <img src={list.it_img1} className={menu.menuListImg} alt={list.it_name} />
+                          </Box>
+                        }
+                        <Box className={menu.menuListTextWrap}>
+                          <Box className={menu.menuListTitle}>
+                            <Typography component="p" mr={1} fontWeight="bold">{list.ca_name}</Typography>
+                            {list.it_type1 === '1' ? <span className={menu.menuListLabel01}>대표메뉴</span> : null}
+                            {list.it_use === '1' ? <span className={menu.menuListLabel02}>판매중</span> : <span className={menu.menuListLabel03}>판매중지</span>}
+                          </Box>
+                          <p className={menu.menuListName}>{list.it_name}</p>
+                          <p className={menu.menuListPrice}>{Api.comma(list.it_price)}원</p>
                         </Box>
-                      }
-                      <Box className={menu.menuListTextWrap}>
-                        <Box className={menu.menuListTitle}>
-                          <Typography component="p" mr={1} fontWeight="bold">{list.ca_name}</Typography>
-                          {list.it_type1 === '1' ? <span className={menu.menuListLabel01}>대표메뉴</span> : null}
-                          {list.it_use === '1' ? <span className={menu.menuListLabel02}>판매중</span> : <span className={menu.menuListLabel03}>판매중지</span>}
-                        </Box>
-                        <p className={menu.menuListName}>{list.it_name}</p>
-                        <p className={menu.menuListPrice}>{Api.comma(list.it_price)}원</p>
                       </Box>
-                    </Box>
-                  </Paper>
-                </Link>
-              </Grid>
-            ))}
-            {lists.length === 0 || lists === null ?
-              <Box style={{ display: 'flex', flex: 1, minHeight: 550, justifyContent: 'center', alignItems: 'center' }}>
-                <Typography style={{ fontSize: 15 }}>등록된 메뉴가 없습니다.</Typography>
-              </Box>
-              : null}
-          </Grid>
-          <Box mt={7} display='flex' justifyContent='center' alignSelf="center">
-            <Stack spacing={2}>
-              <Pagination
-                color="primary"
-                count={totalCount}
-                defaultPage={1}
-                showFirstButton
-                showLastButton
-                onChange={pageHandleChange}
-                page={currentPage}
-              />
-              {/* 
+                    </Paper>
+                  </Link>
+                </Grid>
+              ))}
+            </Grid>
+          }
+          {lists.length === 0 || lists === null ?
+            <Box style={{ display: 'flex', flex: 1, minHeight: 550, justifyContent: 'center', alignItems: 'center' }}>
+              <Typography style={{ fontSize: 15 }}>등록된 메뉴가 없습니다.</Typography>
+            </Box>
+            : null}
+          {totalCount ?
+            <Box mt={7} display='flex' justifyContent='center' alignSelf="center">
+              <Stack spacing={2}>
+                <Pagination
+                  color="primary"
+                  count={totalCount}
+                  defaultPage={1}
+                  showFirstButton
+                  showLastButton
+                  onChange={pageHandleChange}
+                  page={currentPage}
+                />
+                {/* 
                 토탈 페이지수 = count
                 초기 페이지 번호 = defaultPage
               */}
-            </Stack>
-          </Box>
+              </Stack>
+            </Box>
+            : null}
         </MainBox>
       }
     </Box>
