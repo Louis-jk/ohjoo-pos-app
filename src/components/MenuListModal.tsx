0be81428fa975fa5c881652b0b595a149d9b7ad3
Juno Kim <lippoint.surf0622@gmail.com>
2022-01-17 18:28:01 +0900

    로딩바를 달았다가 로딩바 해제하고 데이터 다 불러왔을 때 모달 활성화로 변경

--- src/components/MenuListModal.tsx
+++ src/components/MenuListModal.tsx
@@ -11,10 +11,12 @@ import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 
+// Material icons
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 // Local Components
 import Api from '../Api';
-import { baseStyles } from '../styles/base';
+import { MainBox, baseStyles } from '../styles/base';
 import { MenuStyles } from '../styles/custom';
 import { ModalCancelButton, ModalConfirmButton } from '../styles/customButtons';
 
@@ -31,6 +33,7 @@ const MenuListModal: React.FC<Props> = ({ open, onClose, reflesh, propModalToast
   const menuSt = MenuStyles();
   const { mt_id, mt_jumju_code } = useSelector((state: any) => state.login);
   const [menus, setMenus] = useState<any[]>([]);
+  const [isLoading, setLoading] = useState<boolean>(true);
 
 
   // 메뉴 전체 불러오기
@@ -47,8 +50,10 @@ const MenuListModal: React.FC<Props> = ({ open, onClose, reflesh, propModalToast
 
       if (resultItem.result === 'Y') {
         setMenus(arrItems);
+        setLoading(false);
       } else {
         setMenus([]);
+        setLoading(false);
       }
     });
 
@@ -103,89 +108,94 @@ const MenuListModal: React.FC<Props> = ({ open, onClose, reflesh, propModalToast
     });
   }
 
+
   return (
+    <>
+      {
+        menus && menus.length > 0 ?
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            className={base.modal}
+            open={open}
+            // onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+            <Fade in={open}>
+              <Box className={base.modalInner} style={{ backgroundColor: '#25233e', width: 500 }}>
+                <Box mb={2}>
+                  <Typography component="h2" id="transition-modal-title" style={{ fontSize: 20 }} color='primary' fontWeight='bold'>메뉴 리스트</Typography>
+                </Box>
+                <Paper className={base.paper}>
+                  <Box style={{ maxHeight: 500, overflow: 'auto' }}>
+                    {menus && menus.length > 0 &&
+                      menus.map((menu: any, index: number) => (
+                        <Box key={`menus-${index}`} display='flex' flexDirection='row' justifyContent='space-between' alignItems='center' mb={1.1}>
+                          <Box display='flex' flexDirection='row' justifyContent='flex-start' alignItems='center'>
+                            {menu.it_img1 ?
+                              <img src={menu.it_img1} style={{ width: 65, height: 65, objectFit: 'cover', marginRight: 15 }} alt={`${menu.ca_name}의 썸네일`} title={`${menu.ca_name}의 썸네일`} />
+                              :
+                              <Box style={{ display: 'block', width: 65, height: 65, backgroundColor: '#eee', marginRight: 15 }} />
+                            }
+                            <Box display='flex' flexDirection='column' justifyContent='flex-start' alignItems='flex-start'>
+                              <Box className={menuSt.menuListTitle}>
+                                <Typography component="p" mr={1} fontWeight="bold" fontSize={13}>{menu.ca_name}</Typography>
+                                {menu.it_type1 === '1' ? <span className={menuSt.menuListLabel01}>대표메뉴</span> : null}
+                                {menu.it_use === '1' ? <span className={menuSt.menuListLabel02}>판매중</span> : <span className={menuSt.menuListLabel03}>판매중지</span>}
+                                {menu.it_soldout === '1' ? <span className={menuSt.menuListLabel05}>품절</span> : null}
+                              </Box>
+                              <Box className={menuSt.menuListTitle}>
+                                <Typography fontSize={12}>{menu.it_name}</Typography>
+                              </Box>
+                              <Typography fontSize={12}>{Api.comma(menu.it_price)}원</Typography>
+                            </Box>
+                          </Box>
 
-    <Modal
-      aria-labelledby="transition-modal-title"
-      aria-describedby="transition-modal-description"
-      className={base.modal}
-      open={open}
-      // onClose={handleClose}
-      closeAfterTransition
-      BackdropComponent={Backdrop}
-      BackdropProps={{
-        timeout: 500,
-      }}
-    >
-      <Fade in={open}>
-        <Box className={base.modalInner} style={{ backgroundColor: '#25233e', width: 500 }}>
-          <Box mb={2}>
-            <Typography component="h2" id="transition-modal-title" style={{ fontSize: 20 }} color='primary' fontWeight='bold'>메뉴 리스트</Typography>
-          </Box>
-          <Paper className={base.paper}>
-            <Box style={{ maxHeight: 500, overflow: 'auto' }}>
-              {menus && menus.length > 0 &&
-                menus.map((menu: any, index: number) => (
-                  <Box key={`menus-${index}`} display='flex' flexDirection='row' justifyContent='space-between' alignItems='center' mb={1.1}>
-                    <Box display='flex' flexDirection='row' justifyContent='flex-start' alignItems='center'>
-                      {menu.it_img1 ?
-                        <img src={menu.it_img1} style={{ width: 65, height: 65, objectFit: 'cover', marginRight: 15 }} alt={`${menu.ca_name}의 썸네일`} title={`${menu.ca_name}의 썸네일`} />
-                        :
-                        <Box style={{ display: 'block', width: 65, height: 65, backgroundColor: '#eee', marginRight: 15 }} />
-                      }
-                      <Box display='flex' flexDirection='column' justifyContent='flex-start' alignItems='flex-start'>
-                        <Box className={menuSt.menuListTitle}>
-                          <Typography component="p" mr={1} fontWeight="bold" fontSize={13}>{menu.ca_name}</Typography>
-                          {menu.it_type1 === '1' ? <span className={menuSt.menuListLabel01}>대표메뉴</span> : null}
-                          {menu.it_use === '1' ? <span className={menuSt.menuListLabel02}>판매중</span> : <span className={menuSt.menuListLabel03}>판매중지</span>}
-                          {menu.it_soldout === '1' ? <span className={menuSt.menuListLabel05}>품절</span> : null}
+                          <input
+                            type="text"
+                            value={menu.it_order}
+                            style={{
+                              width: 50,
+                              padding: 5,
+                              textAlign: 'center',
+                              border: 'none',
+                              borderWidth: 1,
+                              borderStyle: 'solid',
+                              borderColor: '#c4c4c4',
+                              borderRadius: 3
+                            }}
+                            onChange={(e: any) => {
+                              const re = /^[0-9\b]+$/;
+                              if (e.target.value === '' || re.test(e.target.value)) {
+                                let changed = e.target.value.replace(/(^0+)/, '');
+                                setMenus(prev => {
+                                  const result = [...prev];
+                                  result[index].it_order = changed;
+                                  return result;
+                                })
+                              }
+                            }}
+                          />
+
                         </Box>
-                        <Box className={menuSt.menuListTitle}>
-                          <Typography fontSize={12}>{menu.it_name}</Typography>
-                        </Box>
-                        <Typography fontSize={12}>{Api.comma(menu.it_price)}원</Typography>
-                      </Box>
-                    </Box>
-
-                    <input
-                      type="text"
-                      value={menu.it_order}
-                      style={{
-                        width: 50,
-                        padding: 5,
-                        textAlign: 'center',
-                        border: 'none',
-                        borderWidth: 1,
-                        borderStyle: 'solid',
-                        borderColor: '#c4c4c4',
-                        borderRadius: 3
-                      }}
-                      onChange={(e: any) => {
-                        const re = /^[0-9\b]+$/;
-                        if (e.target.value === '' || re.test(e.target.value)) {
-                          let changed = e.target.value.replace(/(^0+)/, '');
-                          setMenus(prev => {
-                            const result = [...prev];
-                            result[index].it_order = changed;
-                            return result;
-                          })
-                        }
-                      }}
-                    />
-
+                      ))
+                    }
                   </Box>
-                ))
-              }
-            </Box>
-          </Paper>
-          <ButtonGroup variant="text" color="primary" aria-label="text primary button group" style={{ marginTop: 20 }}>
-            <ModalConfirmButton variant="contained" style={{ boxShadow: 'none' }} onClick={onSumbmit}>수정하기</ModalConfirmButton>
-            <ModalCancelButton variant="outlined" onClick={onClose}>닫기</ModalCancelButton>
-          </ButtonGroup>
-        </Box>
-      </Fade>
-    </Modal>
-
+                </Paper>
+                <ButtonGroup variant="text" color="primary" aria-label="text primary button group" style={{ marginTop: 20 }}>
+                  <ModalConfirmButton variant="contained" style={{ boxShadow: 'none' }} onClick={onSumbmit}>수정하기</ModalConfirmButton>
+                  <ModalCancelButton variant="outlined" onClick={onClose}>닫기</ModalCancelButton>
+                </ButtonGroup>
+              </Box>
+            </Fade>
+          </Modal>
+          : null
+      }
+    </>
   )
 }
 
